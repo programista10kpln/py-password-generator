08db4a67663a3fea50f233d26d1193528b5c6611
zadymiarz <programista10kpln@proton.me>
2022-08-19 13:40:33 +0200

    version 0.1

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ from random import randint
 characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
 characters_list = [character for character in characters]
 
-length = input('how long you want your new password?\n> ')
+length = int(input('how long you want your new password?\n> ')) + 1
+
+passwd = []
+
+for i in range(1, length):
+    passwd.append(characters_list[randint(0, len(characters_list) - 1)])
+
+passwd_text = ''.join(passwd)
+print(passwd_text)
